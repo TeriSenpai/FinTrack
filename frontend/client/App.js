3ceec26297e82777b6d4b840a7462d5da270e7aa
TeriSenpai <gederten9999@gmail.com>
2024-05-01 23:45:24 +1000

    Update

---
+++ frontend/client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Добро пожаловать в FinTrack</h1>
+      <p>Здесь будет интерфейс приложения для управления личными финансами.</p>
+    </div>
+  );
+}
+
+export default App;
